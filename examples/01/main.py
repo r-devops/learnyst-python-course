31c365eb33ec4fb310b6fa5d5b114557f16b3f4b
SVR Kalluru <raghuk.vit@gmail.com>
2025-02-08 23:35:55 +0530

    Update Scripts

---
+++ examples/01/main.py
@@ -0,0 +1,5 @@
+# Get the current CPU and Memory Usage in the system, Plus top 3 CPU & Memory Process.
+import psutil
+
+if __name__ == '__main__':
+
---
+++ examples/01/test-scripts/01.py
@@ -0,0 +1,3 @@
+import psutil
+
+print(dir(psutil))
