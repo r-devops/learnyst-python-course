9277cc0647cbd4e709f5b62300859ecb248ce08e
SVR Kalluru <raghuk.vit@gmail.com>
2025-02-09 20:26:52 +0530

    Update Scripts

--- examples/02/main.py
+++ examples/02/main.py
@@ -32,7 +32,7 @@ def create_ec2_instance(sg_id):
         MaxCount = 1,
         MinCount = 1,
         ImageId='ami-09c813fb71547fc4f',
-        InstanceType='t3.mciro',
+        InstanceType='t3.micro',
         SecurityGroupIds=[
             'string',
         ],
