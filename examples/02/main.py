2eeaeb2814f040aaa58f0e15d9d021f12076e251
SVR Kalluru <raghuk.vit@gmail.com>
2025-02-09 20:18:15 +0530

    Update Scripts

--- examples/02/main.py
+++ examples/02/main.py
@@ -8,6 +8,22 @@ def create_security_group():
         Description='boto3_example',
         GroupName='boto3_example'
     )
+    response_for_ingress = client.authorize_security_group_ingress(
+        GroupId=response["GroupId"],
+        IpPermissions=[
+            {
+                'IpProtocol': 'tcp',
+                'FromPort': 22,
+                'ToPort': 22,
+                'IpRanges': [
+                    {
+                        'Description': 'Allow SSH',
+                        'CidrIp': '0.0.0.0/0',
+                    },
+                ]
+            },
+        ],
+    )
 
 if __name__ == '__main__':
     create_security_group()
